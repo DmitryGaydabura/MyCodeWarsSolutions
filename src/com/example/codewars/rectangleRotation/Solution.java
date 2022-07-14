51829c0fda8f98a433ce398862a17c61b2c3383f
DmitryGaydabura <gaydabura.d@icloud.com>
2022-07-14 23:00:01 +0300

    Human Readable Time added

--- src/com/example/codewars/rectangleRotation/Solution.java
+++ src/com/example/codewars/rectangleRotation/Solution.java
@@ -1,10 +1,9 @@
 package com.example.codewars.rectangleRotation;
 
-
 public class Solution {
     public static void main(String[] args) {
         long timeStart = System.currentTimeMillis();
-        //Record ~1100 ms for (10000 , 10000)
+        //Record ~1050 ms for (10000 , 10000)
         System.out.println(rectangleRotation(10000, 10000));
         System.out.println(System.currentTimeMillis() - timeStart);
     }
@@ -17,48 +16,31 @@ public class Solution {
         float x = (float) (Math.cos(Math.toRadians(alpha)) * c);
         float y = (float) (Math.sin(Math.toRadians(alpha)) * c);
 
-        float[] first = {x, y};
-        float[] second = {y, x};
-        float[] third = {-x, -y};
-        float[] fourth = {-y, -x};
 
         //total number of points
         int number = 0;
 
-
         // Checking if the point is inside the rectangle.
+
         for (int i = (int) -x; i < x; i++) {
             for (int j = (int) -x; j < x; j++) {
-                // Creating a new array with the values of i and j.
-                float[] dot = {i, j};
                 // Checking if the point is inside the rectangle.
-                boolean c1 = check(first, second, dot, 1);
+                boolean c1 = check(x, y, y, x, i, j, 1);
                 if (!c1) continue;
-                boolean c2 = check(second, third, dot, 0);
+                boolean c2 = check(y, x, -x, -y, i, j, 0);
                 if (!c2) continue;
-                boolean c3 = check(third, fourth, dot, 1);
+                boolean c3 = check(-x, -y, -y, -x, i, j, 1);
                 if (!c3) continue;
-                boolean c4 = check(fourth, first, dot, 0);
+                boolean c4 = check(-y, -x, x, y, i, j, 0);
                 if (!c4) continue;
                 number++;
 
             }
         }
-
         return number;
     }
 
-    private static boolean check(float[] first, float[] second, float[] dot, int n) {
-
-        float x = dot[0];
-        float y = dot[1];
-
-        float x1 = first[0];
-        float y1 = first[1];
-
-        float x2 = second[0];
-        float y2 = second[1];
-
+    private static boolean check(float x1, float y1, float x2, float y2, int x, int y, int n) {
         return n == 1 ? ((x - x1) / (x2 - x1)) >= ((y - y1) / (y2 - y1)) : ((x - x1) / (x2 - x1)) <= ((y - y1) / (y2 - y1));
     }
 
