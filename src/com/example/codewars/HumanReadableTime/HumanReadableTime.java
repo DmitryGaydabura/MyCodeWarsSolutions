58063f3a7604f3e569f8a358fa3d45d80e584727
DmitryGaydabura <gaydabura.d@icloud.com>
2022-07-14 18:47:26 +0300

    Human Readable Time added

---
+++ src/com/example/codewars/HumanReadableTime/HumanReadableTime.java
@@ -0,0 +1,40 @@
+package com.example.codewars.HumanReadableTime;
+
+public class HumanReadableTime {
+
+    public static void main(String[] args) {
+
+        System.out.println(makeReadable(1500));
+    }
+    public static String makeReadable(int seconds) {
+        // Do something
+        String secondString;
+        String minuteString;
+        String hourString;
+
+        int hours = seconds / 3600;
+        if (hours > 9) {
+            hourString = ""+ hours;
+        } else {
+            hourString = "0"+ hours;
+        }
+        seconds %= 3600;
+
+        int minute = seconds /60;
+        if (minute > 9){
+            minuteString = ""+ minute;
+        }else{
+            minuteString = "0"+ minute;
+        }
+        seconds %= 60;
+
+        int second = seconds;
+        if (second > 9){
+            secondString = ""+ second;
+        }else{
+            secondString = "0"+ second;
+        }
+
+        return hourString + ":" + minuteString + ":" + secondString;
+    }
+}
--- src/com/example/codewars/rectangleRotation/Solution.java
+++ src/com/example/codewars/rectangleRotation/Solution.java
@@ -4,28 +4,23 @@ package com.example.codewars.rectangleRotation;
 public class Solution {
     public static void main(String[] args) {
         long timeStart = System.currentTimeMillis();
-        System.out.println(rectangleRotation(6, 4));
+        //Record ~1100 ms for (10000 , 10000)
+        System.out.println(rectangleRotation(10000, 10000));
         System.out.println(System.currentTimeMillis() - timeStart);
     }
 
-    static int rectangleRotation( int a,  int b) {
+    static int rectangleRotation(int a, int b) {
 
-        // Calculating the diagonal of the rectangle.
-        double c = Math.sqrt(a * a + b * b) / 2;
-        // Calculating the angle between the diagonal and the side of the rectangle.
-        double gamma = Math.toDegrees(Math.acos((c * c + c * c - b * b) / (2 * c * c)));
-        // Calculating the angle between the diagonal and the side of the rectangle.
-        double alpha = (90 - gamma) / 2;
-        // Calculating the length of the side of the rectangle.
-        double x = Math.cos(Math.toRadians(alpha)) * c;
-        // Calculating the length of the side of the rectangle.
-        double y = Math.sin(Math.toRadians(alpha)) * c;
+        float c = (float) (Math.sqrt(a * a + b * b) / 2);
+        float gamma = (float) Math.toDegrees(Math.acos((c * c + c * c - b * b) / (2 * c * c)));
+        float alpha = ((90 - gamma) / 2);
+        float x = (float) (Math.cos(Math.toRadians(alpha)) * c);
+        float y = (float) (Math.sin(Math.toRadians(alpha)) * c);
 
-        //Coordinates of Rectangle points
-        double[] first = {x, y};
-        double[] second = {y, x};
-        double[] third = {-x, -y};
-        double[] fourth = {-y, -x};
+        float[] first = {x, y};
+        float[] second = {y, x};
+        float[] third = {-x, -y};
+        float[] fourth = {-y, -x};
 
         //total number of points
         int number = 0;
@@ -35,27 +30,34 @@ public class Solution {
         for (int i = (int) -x; i < x; i++) {
             for (int j = (int) -x; j < x; j++) {
                 // Creating a new array with the values of i and j.
-                double[] dot = {i, j};
+                float[] dot = {i, j};
                 // Checking if the point is inside the rectangle.
-                if (check(first, second, dot, 1) && check(second, third, dot, 0) && check(third, fourth, dot, 1) && check(fourth, first, dot, 0)) {
-                    number++;
-                }
+                boolean c1 = check(first, second, dot, 1);
+                if (!c1) continue;
+                boolean c2 = check(second, third, dot, 0);
+                if (!c2) continue;
+                boolean c3 = check(third, fourth, dot, 1);
+                if (!c3) continue;
+                boolean c4 = check(fourth, first, dot, 0);
+                if (!c4) continue;
+                number++;
+
             }
         }
 
         return number;
     }
 
-    private static boolean check(double[] first, double[] second, double[] dot, int n) {
+    private static boolean check(float[] first, float[] second, float[] dot, int n) {
 
-        double x = dot[0];
-        double y = dot[1];
+        float x = dot[0];
+        float y = dot[1];
 
-        double x1 = first[0];
-        double y1 = first[1];
+        float x1 = first[0];
+        float y1 = first[1];
 
-        double x2 = second[0];
-        double y2 = second[1];
+        float x2 = second[0];
+        float y2 = second[1];
 
         return n == 1 ? ((x - x1) / (x2 - x1)) >= ((y - y1) / (y2 - y1)) : ((x - x1) / (x2 - x1)) <= ((y - y1) / (y2 - y1));
     }
